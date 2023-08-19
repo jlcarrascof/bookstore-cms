fa1efda5e597bc472b0a4dc59a9e3c6cbd9536c9
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-08-19 11:52:41 -0400

    Fix Routes according to my code reviewer suggestions and deleted the Home component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Categories from './components/Categories';
 import Navbar from './components/Navbar';
 import Book from './components/Book';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Book />,
-  },
-  {
-    path: '/categories',
-    element: <Categories />,
-  },
-]);
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <h1>Bookstore</h1>
-        <RouterProvider router={router} />
-      </header>
+      <Router>
+        <header className="App-header">
+          <Navbar />
+          <h1>Bookstore</h1>
+          <Routes>
+            <Route path="/" element={<Book />} />
+            <Route path="/categories" element={<Categories />} />
+          </Routes>
+        </header>
+      </Router>
     </div>
   );
 }
